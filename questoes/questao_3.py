e20799e9257b5cce0f6ad12082bcd274f55b115f
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-22 00:00:19 -0300

    Add files via upload

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -1,46 +1,17 @@
-## QUESTÃO 3 ##
-#
-# Um robô se move em um plano a partir do ponto original (0,0). O robô pode se 
-# mover nas direções CIMA, BAIXO, ESQUERDA e DIREITA de acordo com um 
-# passo fornecido. O traço do movimento do robô é mostrado da seguinte forma:
-#
-# CIMA 5
-# BAIXO 3
-# ESQUERDA 3
-# DIREITA 2
-#
-# Os números após a direção são passos. 
-# Escreva um programa para calcular a distância entre a posição atual e o 
-# ponto original após uma seqüência de movimentos. Se a distância for um 
-# float, basta imprimir o inteiro mais próximo.
-# Exemplo:
-# Se as seguintes tuplas são dadas como entrada para o programa:
-# 
-# CIMA 5
-# BAIXO 3
-# ESQUERDA 3
-# DIREITA 2
-#
-# Então, a saída do programa deve ser:
-# 2
-# 
-# Dicas:
-# As entradas devem ser lidas do console até que um valor vazio seja digitado. 
-# A saída deve ser um inteiro que representa a distancia para o ponto original. 
-# Entradas inválidas devem ser descartadas da contagem.
-##
-
-
-##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
-# Deve-se substituir o comado print existente pelo código da solução.
-# Para a correta execução do programa, a estrutura atual deve ser mantida,
-# substituindo apenas o comando print(questão...) existente.
-##
 def main():
-    print("questao 3")
 
-
+    up, down, left, right = (int (x) for x in (0,0,0,0))
     
+    while True:
+        movimento = input('Digite os movimentos cima, baixo, esquerda, direta (do robo), separdos por virgula').split(',')
+        if movimento == '':
+            break
+        else:
+            continue
+        def calc_dis(movimento):
+            distancia =
+            rounded = ((down - up)**2 + (left - right)**2 )**(1/2)
+            print(round(rounded))
+
 if __name__ == '__main__':
     main()
