c68f4f75f783a0f7f9c1d779f5234f215cbe0d65
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-22 04:07:02 -0300

    Add files via upload

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -1,53 +1,22 @@
-## QUESTÃO 5 ##
-#
-# Crie uma implementação da cifra rotacional, às vezes também chamada de cifra de César.
-# 
-# A cifra de César é uma simples cifra de deslocamento que se baseia na transposição 
-# de todas as letras do alfabeto usando uma chave inteira entre 0 e 26. 
-# O uso da chave 0 ou 26 sempre produzirá a mesma saída devido à aritmética modular. 
-# A letra é deslocada para tantos valores quanto o valor da chave.
-#
-# A notação geral para cifras rotacionais é ROT + <chave>. A cifra rotacional mais comumente usada é a ROT13.
-#
-# Um ROT13 no alfabeto latino seria o seguinte:
-# 	Normal:  abcdefghijklmnopqrstuvwxyz
-#	Cifrado: nopqrstuvwxyzabcdefghijklm
-#
-# Exemplos:
-#	Entrada: ROT5 omg 
-#          Saída: trl
-#	Entrada: ROT0 c 
-#          Saída: c
-#	Entrada: ROT26 Cool 
-#          Saída: Cool
-#	Entrada: ROT13 The quick brown fox jumps over the lazy dog. 
-#          Saída: Gur dhvpx oebja sbk whzcf bire gur ynml qbt.
-#	Entrada: ROT13 Gur dhvpx oebja sbk whzcf bire gur ynml qbt. 
-#          Saída: The quick brown fox jumps over the lazy dog.
-#
-# Se um valor de entrada inválido for digitado, a seguinte mensagem 
-# deve ser impressa: 'Erro'. 
-# Entradas inválidas podem ser entradas que contém códigos de rotações 
-# inválidos ou mensagens contendo caracteres que não estão no alfabeto. 
-# Exemplos:
-# 	Entrada: RARA abc Saída: Erro
-# 	Entrada: ROT5 c99 Saída: Erro
-#
-# As entradas devem ser sempre compostas por ROT + <chave> + ' ' + 'mensagem', 
-# ou seja, a cifra rotacional e a mensagem a ser cifrada separados por vírgula.
-##
 
-
-##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
-# Deve-se substituir o comado print existente pelo código da solução.
-# Para a correta execução do programa, a estrutura atual deve ser mantida,
-# substituindo apenas o comando print(questão...) existente.
-##
 def main():
-    print("questao 5")
 
+    rot_in = input("Digite aqui a cifra rotacional ROT + <chave> + ' ' + mensagem: ").split(' ', 1)
 
-    
+    for item in rot_in:
+        if not "ROT" in item:
+            print("erro")
+            return
+        else:
+            rot_message = rot_in[1]
+            rot_key = int(rot_in[0].split('ROT')[1])
+            alphabet_latin = 'abcdefghijklmnopqrstuvwxyz'
+                
+            def caesar_cypher(rot_message, rot_key):
+                alphabet_caesar = alphabet_latin[rot_key:] + alphabet_latin[:rot_key]
+                trans_ref = str.maketrans(alphabet_latin, alphabet_caesar)
+                print(rot_message.translate(trans_ref))
+            caesar_cypher(rot_message, rot_key)
+
 if __name__ == '__main__':
     main()
