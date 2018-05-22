29fd10e0f42b1055472cf6057d0aa1d3fd5756e4
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-21 23:39:58 -0300

    Add files via upload

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -1,36 +1,48 @@
-## QUESTÃO 1 ##
-#
-# Um site exige que os usuários insiram nome de usuário e senha para se registrarem. 
-# Escreva um programa para verificar se a senha digitada pelos usuários é forte o suficiente.
-# A seguir estão os critérios para verificar a senha:
-#
-# 1. Pelo menos uma letra entre [a-z]
-# 2. Pelo menos 1 número entre [0-9]
-# 3. Pelo menos uma letra entre [A-Z]
-# 4. Pelo menos 1 caractere de [$ # @]
-# 5. Comprimento mínimo da senha: 6
-# 6. Comprimento máximo da senha: 12
-#
-# Seu programa deve aceitar uma sequência de senhas separadas por vírgula e as verificará 
-# de acordo com os critérios acima. As senhas que correspondem aos critérios devem ser 
-# impressas, separadas por uma vírgula.
-# Exemplo
-# Se as seguintes senhas forem fornecidas como entrada para o programa:
-# ABd1234 @ 1, um F1 #, 2w3E *, 2We3345
-# Então, a saída do programa deve ser:
-# ABd1234 @ 1
-##
 
-##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
-# Deve-se substituir o comado print existente pelo código da solução.
-# Para a correta execução do programa, a estrutura atual deve ser mantida,
-# substituindo apenas o comando print(questão...) existente.
-##
 def main():
-    print("questao 1")
-    
 
+    def password_is_valid(password):
+
+        def len_is_valid(password):
+                if len(password) >= 6 and len(password) <= 12:
+                    return True
+        def numeric_is_valid(password):
+            for char in password:
+                if char.isnumeric() == True:
+                    return True
+
+        def alpha_is_valid(password):
+            for char in password:
+                if char.isalpha() == True:
+                    return True     
+
+        def upper_is_valid(password):
+            for char in password:
+                if char.isupper() == True:
+                    return True
+
+        def lower_is_valid(password):
+            for char in password:
+                if char.islower() == True:
+                    return True
+
+        def special_is_valid(password):
+            for char in password:
+                if char == '@' or char == '#' or char == '$':
+                    return True
+
+        if len_is_valid(password) and numeric_is_valid(password) and alpha_is_valid(password) and upper_is_valid(password) and lower_is_valid(password) and special_is_valid(password): 
+            return True
+
+    password_list = input('Digite uma lista de senhas, separado por virgula: ').split(',')
+    valid_passwords = []
+
+    for password in password_list:
+            password_is_valid(password)
+        if password_is_valid(password) == True:
+            valid_passwords.append(password)
+
+    print('As senhas validas sao: ', valid_passwords)
 
 if __name__ == '__main__':
     main()
--- questoes/questao_2.py
+++ questoes/questao_2.py
@@ -1,35 +1,12 @@
-## QUESTÃO 2 ##
-#
-# Escreva um programa para calcular a frequencia das palavras de uma entrada. 
-# A saída deve mostrar a frequencia depois de ordenar a chave alfanumericamente.
-# Suponha que a seguinte entrada seja fornecida ao programa:
-#     New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
-#
-# Então, a saída deve ser:
-# 2:2
-# 3.:1
-# 3?:1
-# New:1
-# Python:5
-# Read:1
-# and:1
-# between:1
-# choosing:1
-# or:2
-# to:1
-##
-
-
-##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
-# Deve-se substituir o comado print existente pelo código da solução.
-# Para a correta execução do programa, a estrutura atual deve ser mantida,
-# substituindo apenas o comando print(questão...) existente.
-##
 def main():
-    print("questao 2")
+    
+    user_in = input('Digite qualquer coisa aqui: ').split()
+    frequencies = []
 
+    for char in user_in:
+        frequencies.append("{}:{}".format(char,user_in.count(char)))
 
+    print(sorted(frequencies))
 
 if __name__ == '__main__':
     main()
