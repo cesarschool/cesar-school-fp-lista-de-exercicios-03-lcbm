9df5f8241dcda5d08bfc317ec4a408a00e88e4a8
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-22 02:15:39 -0300

    Add files via upload

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -1,42 +1,13 @@
-## QUESTÃO 4 ##
-#
-# Você foi solicitado a escrever um programa para classificar tuplas (nome, idade, altura) 
-# por ordem crescente, em que nome é uma string, idade e altura são números. As tuplas
-# são inseridas pelo console. O critério de classificação é:
-#
-# 1: Ordenar com base no nome;
-# 2: Em seguida, classifique com base na idade;
-# 3: Em seguida, classifique por pontuação.
-#
-# A prioridade é essa nome > idade > pontuação.
-# 
-# Se as seguintes tuplas são dadas como entrada para o programa:
-# Tom 19,80
-# João, 20,90
-# Jony, 17,91
-# Jony, 17,93
-# Json, 21,85
-#
-# Então, a saída do programa deve ser:
-# [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21 ',' 85 '), (' Tom ',' 19 ',' 80 ')]
-#
-# Dicas:
-# As entradas devem ser lidas do console até que um valor vazio seja digitado. 
-# A saída deve ser impressa no formato apresentado acima.
-# Valores inválidos (fora do formato esperado para uma tupla) devem ser descartados.
-##
-
-
-##
-# A sua resposta da questão deve ser desenvolvida dentro da função main()!!! 
-# Deve-se substituir o comado print existente pelo código da solução.
-# Para a correta execução do programa, a estrutura atual deve ser mantida,
-# substituindo apenas o comando print(questão...) existente.
-##
 def main():
-    print("questao 4")
 
+    tuple_out =[]
+    while True:
+        user_data = input('Nome, idade e altura, separados por virgula: ')
+        if user_data == '':
+            print(sorted(list(zip(tuple_out))))
+            break
+        else:
+            tuple_out.append(user_data)
 
-    
 if __name__ == '__main__':
     main()
