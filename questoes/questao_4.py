145fadad1ce8f3c6ab91fbc3340702c170b18b7d
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-25 13:43:22 -0300

    Update questao_4.py
    Added list comprehension in line 11 in order to correctly split data by commas and remove spaces, from user input.

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -1,13 +1,14 @@
 def main():
-
+    
+    list_in = []
     tuple_out =[]
     while True:
         user_data = input('Nome, idade e altura, separados por virgula: ')
         if user_data == '':
-            print(sorted(list(zip(tuple_out))))
+            print(sorted(list_in))
             break
         else:
-            tuple_out.append(user_data)
+            list_in.append(tuple(item.strip() for item in user_data.split(',')))
 
 if __name__ == '__main__':
     main()
