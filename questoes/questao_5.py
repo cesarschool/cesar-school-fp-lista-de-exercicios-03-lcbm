af246611a58e338d8bf10788c13cb1a83cc4b2a9
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-05-25 14:40:42 -0300

    Update questao_5.py
    Updated line 7 to reject inputs where cypher method is not equal to 'ROT'
    Before, program accepted inputs such as: "ROTA13 abcdefgh...." and "ROT13 abcdefROTghijk"

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -1,12 +1,13 @@
-
 def main():
-
-    rot_in = input("Digite aqui a cifra rotacional ROT + <chave> + ' ' + mensagem: ").split(' ', 1)
-
-    for item in rot_in:
-        if not "ROT" in item:
-            print("erro")
-            return
+    
+    while True:
+        rot_in = input("Digite aqui a cifra rotacional ROT + <chave> + ' ' + mensagem: ").split(' ', 1)
+        cypher = rot_in[0][0:3]
+        
+        if cypher != "ROT" or sum(map(str.isalpha, rot_in[0])) > 3:
+            print("ERRO")
+            continue
+        
         else:
             rot_message = rot_in[1]
             rot_key = int(rot_in[0].split('ROT')[1])
@@ -15,8 +16,9 @@ def main():
             def caesar_cypher(rot_message, rot_key):
                 alphabet_caesar = alphabet_latin[rot_key:] + alphabet_latin[:rot_key]
                 trans_ref = str.maketrans(alphabet_latin, alphabet_caesar)
-                print(rot_message.translate(trans_ref))
+                print("Mensagem cifrada: ", rot_message.translate(trans_ref))
             caesar_cypher(rot_message, rot_key)
-
+            break
+            
 if __name__ == '__main__':
     main()
